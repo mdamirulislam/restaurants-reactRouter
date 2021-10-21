420d7e3c55cf58f93c3425ba1faa4baf916f7f02
mdamirulislam <amirulkhan.khulna.bd@gmail.com>
2021-10-21 11:08:50 +0600

    done

--- src/compomonents/MealDetail/MealDetail.js
+++ src/compomonents/MealDetail/MealDetail.js
@@ -28,7 +28,7 @@ const MealDetail = () => {
                 </div>
                 <div className='col-md-4'>
                     <h2> Name: {strMeal}</h2>
-                    <p>{strInstructions}</p>
+                    <p>{strInstructions.slice(0,500)}</p>
               <a href="https://www.youtube.com/watch?v=CmdTUdljh1Q"><FontAwesomeIcon className='DetailInfoIcon' icon={faVideo}/></a> <br /> <br />
                     <button onClick={handleHistory}>Go Back</button>
                 </div>
